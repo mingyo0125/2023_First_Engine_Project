18c988c67af56e66dc188f3f5237a1abfbbfd954
mingyo0125 <wjdalsry125@gmail.com>
2023-05-16 22:32:56 +0900

    너임마해낸거야임마

--- Assets/01.Scripts/ArrowOnOff.cs
+++ Assets/01.Scripts/ArrowOnOff.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.XR;
 
 public class ArrowOnOff : MonoBehaviour
 {
@@ -13,34 +14,30 @@ public class ArrowOnOff : MonoBehaviour
     private Vector3 _arrowPosition;
 
     int _arrowCount = 0;
-    int[] _randArr;
+    int[] _randArr = new int[5];
 
     private void CreateArrow(int _rand)
     {
-        for (int i = 0; i < 5; i++)
-        {
-            int rand = _rand;
-            _randArr[i] = rand;
+        int rand = _rand;
 
-            switch (rand)
-            {
-                case 1:
-                    Arrow left = PoolManager.Instance.Pop("Left") as Arrow;
-                    _arrowList.Add(left);
-                    break;
-                case 2:
-                    Arrow right = PoolManager.Instance.Pop("Right") as Arrow;
-                    _arrowList.Add(right);
-                    break;
-                case 3:
-                    Arrow up = PoolManager.Instance.Pop("Up") as Arrow;
-                    _arrowList.Add(up);
-                    break;
-                case 4:
-                    Arrow down = PoolManager.Instance.Pop("Down") as Arrow;
-                    _arrowList.Add(down);
-                    break;
-            }
+        switch (rand)
+        {
+            case 1:
+                Arrow left = PoolManager.Instance.Pop("Left") as Arrow;
+                _arrowList.Add(left);
+                break;
+            case 2:
+                Arrow right = PoolManager.Instance.Pop("Right") as Arrow;
+                _arrowList.Add(right);
+                break;
+            case 3:
+                Arrow up = PoolManager.Instance.Pop("Up") as Arrow;
+                _arrowList.Add(up);
+                break;
+            case 4:
+                Arrow down = PoolManager.Instance.Pop("Down") as Arrow;
+                _arrowList.Add(down);
+                break;
         }
 
         for (int i = 0; i < _arrowList.Count; i++)
@@ -59,13 +56,21 @@ public class ArrowOnOff : MonoBehaviour
                 PoolManager.Instance.Push(_arrowList[_arrowCount]);
                 _arrowCount++;
             }
-
-            else if(Input.anyKeyDown && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
+            else if(!Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
             {
                 for(int i = 0; i < _arrowList.Count; i++)
                 {
+                    PoolManager.Instance.Push(_arrowList[i]);
+                }
+
+                _arrowCount = 0;
+                _arrowList.Clear();
+
+                for (int i = 0; i < 5; i++)
+                {
                     CreateArrow(_randArr[i]);
                 }
+
             }
         }
 
@@ -77,6 +82,8 @@ public class ArrowOnOff : MonoBehaviour
             for(int i = 0; i < 5; i++)
             {
                 int rand = UnityEngine.Random.Range(1, 5);
+                _randArr[i] = rand;
+                Debug.Log($"randArr{i} : {rand}");
                 CreateArrow(rand);
             }
         }
