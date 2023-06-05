2d8dcf650163992489f3f6bbbd9860067a5006bb
mingyo0125 <wjdalsry125@gmail.com>
2023-06-05 23:40:20 +0900

    적Idle애니메이션

--- Assets/01.Scripts/Enemy/Enemy.cs
+++ Assets/01.Scripts/Enemy/Enemy.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Enemy : PoolableMono
 {
+    Animator _animator;
+    [SerializeField]
+    List<AnimatorController> _animatorControllerList = new List<AnimatorController>();
+    
     public override void Init()
     {
+        _animator = transform.Find("Visual").GetComponent<Animator>();
+        int rand = Random.Range(0, _animatorControllerList.Count);
+        _animator.runtimeAnimatorController = _animatorControllerList[rand];
     }
 }
--- Assets/01.Scripts/Spawner/EnemySpawner.cs
+++ Assets/01.Scripts/Spawner/EnemySpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public List<Enemy> EnemyList = new List<Enemy>();
-
+    
     private void Start()
     {
         EnemySpawn();
