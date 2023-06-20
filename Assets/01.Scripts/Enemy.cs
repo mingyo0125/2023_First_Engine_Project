98be48c074369bad7ebf8b771593a43e9aa01638
mingyo0125 <wjdalsry125@gmail.com>
2023-06-20 23:43:59 +0900

    버러지들제거

--- Assets/01.Scripts/Enemy.cs
+++ Assets/01.Scripts/Enemy.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 
 public class Enemy : PoolableMono
 {
-    Rigidbody _rigidbody;
 
     public override void Init()
     {
@@ -23,7 +22,6 @@ public class Enemy : PoolableMono
 
     private IEnumerator EnemyDieCorou()
     {
-        //_rigidbody.velocity = new Vector3(transform.position.x, transform.position.y * - 5f, transform.position.z);
         transform.DOMoveY(-1f, 0.5f).SetEase(Ease.InBack);
         yield return new WaitForSeconds(1f);
         EnemySpawner.Instance.EnemyKill();
